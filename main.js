bec13d30bcabee37643b86587af11d50b8eb828f
rorosin <46279702+rorosin@users.noreply.github.com>
2024-03-28 17:09:55 +0300

    Translated everything to english

--- main.js
+++ main.js
@@ -26,14 +26,14 @@ async function formatKey(user) {
     if (user.name.includes("FREE ")) {
         var createDate = Date.parse(user.name.split(" ")[1])
         var expiresAfter_days = Number(user.name.split(" ")[2])
+        var [h, m, s] = user.name.split(" ")[3].split(":").map(Number)
+        createDate = new Date(new Date(createDate).setHours(h, m, s))
         var key = {
             id: user.id,
             country: user.accessUrl.split("@")[1].split(":")[0],
             limit: user.dataLimit?.bytes / 1_000_000_000,
             url: user.accessUrl,
-            created: isNaN(createDate)
-                ? undefined
-                : new Date(createDate).toLocaleDateString("ru-RU"),
+            created: isNaN(createDate) ? undefined : new Date(createDate),
             expiresAfter_days: isNaN(expiresAfter_days)
                 ? undefined
                 : expiresAfter_days,
@@ -63,7 +63,9 @@ async function createNewFreeKey(
         (await outlinevpn.addDataLimit(user.id, limit * 1_000_000_000)) &&
         (await outlinevpn.renameUser(
             user.id,
-            `FREE ${new Date().toLocaleDateString("ru-RU")} ${liveDays}`,
+            `FREE ${new Date().toLocaleDateString(
+                "ru-RU",
+            )} ${liveDays} ${new Date().toLocaleTimeString("ru-RU")}`,
         ))
     var userChanged = await outlinevpn.getUser(user.id)
     return userChanged
@@ -83,11 +85,11 @@ async function sendKey(
         await bot.telegram.sendMessage(
             chatID,
             `${config.countries[key.country]} | ${
-                isNaN(key.limit) ? "Безлимит" : "Лимит " + key.limit + " ГБ"
+                isNaN(key.limit) ? "Unlimited" : "Limit " + key.limit + " GB"
             }${
                 isNaN(key.expiresAfter_days)
                     ? ""
-                    : " | Действует " + key.expiresAfter_days + " дн."
+                    : " | Expires after " + key.expiresAfter_days + " d."
             }\n<code>${key.url}${autoName ? "#" + keyAutoName : ""}</code>${
                 subscribeLink && subscribeLink.text && subscribeLink.link
                     ? '\n\n<a href="' +
@@ -105,11 +107,33 @@ async function sendKey(
     }
 }
 
+async function checkExpirations() {
+    var servers = config.env.outlineServers
+    for (var i in servers) {
+        const server = new OutlineVPN({
+            apiUrl: servers[i].url,
+            fingerprint: servers[i].cert,
+        })
+        var keys = await getAllFreeKeys(server)
+        for (var j in keys) {
+            var key = keys[j]
+            if (key.expiresAfter_days && key.created) {
+                var expiryDate = new Date(key.created)
+                expiryDate.setDate(expiryDate.getDate() + key.expiresAfter_days)
+                if (new Date() > expiryDate) {
+                    await server.deleteUser(key.id)
+                    console.log(
+                        `Key with ID ${key.id} on server ${key.country} has expired and was deleted`,
+                    )
+                }
+            }
+        }
+    }
+}
+
+// checkExpirations()
 var servers = config.env.outlineServers
 var serverIter = 0
-cron.validate(config.keyCreateCron)
-    ? undefined
-    : console.error("Invalid key create expression")
 cron.schedule(config.keyCreateCron, async () => {
     const server = new OutlineVPN({
         apiUrl: servers[serverIter].url,
